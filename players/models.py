de7e6ba1e8131810e025c8e670a1a4e5818baedd
martin.bachhuber <martin.bachhuber@pexon-consulting.de>
2024-05-26 15:55:40 +0200

    initial commit

---
+++ players/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    city = models.CharField(max_length=100, null=True, blank=True)
+    stadium = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Player(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='players')
+    position = models.CharField(max_length=50, null=True, blank=True)
+    nationality = models.CharField(max_length=50, null=True, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class PlayerStats(models.Model):
+    player = models.OneToOneField(Player, on_delete=models.CASCADE, related_name='stats')
+    appearances = models.IntegerField(default=0)
+    goals = models.IntegerField(default=0)
+    assists = models.IntegerField(default=0)
+    tackles = models.IntegerField(default=0)
+    sprints = models.IntegerField(default=0)
+    minutes_played = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"Stats for {self.player.first_name} {self.player.last_name}"
+# Create your models here.
